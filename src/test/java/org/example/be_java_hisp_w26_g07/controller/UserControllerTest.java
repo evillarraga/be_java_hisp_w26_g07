2a2dfe8dffdeb113f44e7f7e269d4cff92e1a364
c4rtex05 <carlosarturocardozo1105@gmail.com>
2024-05-07 09:18:36 -0500

    TEST: Se agrega numberOfSellersFollowed de UserController

--- src/test/java/org/example/be_java_hisp_w26_g07/controller/UserControllerTest.java
+++ src/test/java/org/example/be_java_hisp_w26_g07/controller/UserControllerTest.java
@@ -1,17 +1,54 @@
 package org.example.be_java_hisp_w26_g07.controller;
 
+import org.example.be_java_hisp_w26_g07.dto.users.CountFollowersResponseDto;
+import org.example.be_java_hisp_w26_g07.entity.User;
+import org.example.be_java_hisp_w26_g07.service.interfaces.IUserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
+
+    @Mock
+    IUserService userService;
+
+    @InjectMocks
+    UserController userController;
 
     @Test
     void follow() {
     }
 
     @Test
-    void numberOfSellersFollowed() {
+    @DisplayName("Verifica el controlador que devuelva un ResponseEntity con Status Code OK")
+    void numberOfSellersFollowedTest() {
+        //Arrange
+        //Creación de usuario y id´s de seguidores que pertenecen al vendedor
+        Integer userId = 1;
+        User userMock = new User(userId, "Juan", true);
+        CountFollowersResponseDto countFollowersResponseDto = new CountFollowersResponseDto(userId, userMock.getName(),
+                5);
+        ResponseEntity<CountFollowersResponseDto> expected = new ResponseEntity<>(
+                countFollowersResponseDto, HttpStatus.OK);
+
+        //Act
+        when(userService.getNumberOfSellersFollowed(userId)).thenReturn(countFollowersResponseDto);
+
+        ResponseEntity<CountFollowersResponseDto> output = userController.numberOfSellersFollowed(userId);
+
+        //Assert
+        assertEquals(expected, output);
+        assertEquals(HttpStatus.OK, output.getStatusCode());
     }
 
     @Test
